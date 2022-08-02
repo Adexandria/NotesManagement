f98d0f73323893e1ac7279d892e5f1664ffd3a90
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-08-02 22:01:18 +0100

    Added Db layer

---
+++ CCSA_Web.Db/Mappings/NoteMap.cs
@@ -0,0 +1,20 @@
+﻿using CCSA_Web.Domain.Models;
+using FluentNHibernate.Mapping;
+
+namespace CCSA_Web.Db.Mappings
+{
+    public class NoteMap :ClassMap<Note>
+    {
+        public NoteMap()
+        {
+            Id(x => x.Id);
+            Map(x => x.Title);
+            Map(x => x.Content);
+            Map(x => x.CreatedDate);
+            Map(x => x.UpdatedDate);
+            Map(x => x.GroupName).CustomType<Group>();
+            References(x => x.User).Column("UserId");
+
+        }
+    }
+}
---
+++ CCSA_Web.Db/Mappings/UserMap.cs
@@ -0,0 +1,17 @@
+﻿using CCSA_Web.Domain.Models;
+using FluentNHibernate.Mapping;
+
+namespace CCSA_Web.Db.Mappings
+{
+    public class UserMap : ClassMap<User>
+    {
+        public UserMap()
+        {
+            Id(x => x.Id);
+            Map(x => x.Username);
+            Map(x => x.Password);
+            Map(x => x.Email);
+            
+        }
+    }
+}
